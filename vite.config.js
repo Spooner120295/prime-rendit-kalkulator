4e74ac30553b1d6b63f470d1f4bcce5e7a154eb2
Spooner120295 <wittermann1@gmx.de>
2025-09-05 16:38:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/prime-rendit-kalkulator/',   // 👉 HIER eingefügt!
   server: {
     allowedHosts: true
   },
@@ -21,4 +17,4 @@ export default defineConfig({
       },
     },
   },
-}) 
+})
