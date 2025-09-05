73d21cb700f6de477a262e55c8e9a8489fe45ccc
Spooner120295 <wittermann1@gmx.de>
2025-09-05 17:21:58 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  // WICHTIG: Repo-Name hier eintragen
+  // WICHTIG: Repo-Name hier eintragen, damit GitHub Pages Pfade stimmen
   base: '/prime-rendit-kalkulator/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
+      '@': path.resolve(__dirname, './src')
+    }
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: { '.js': 'jsx' }
+    }
+  }
 })
